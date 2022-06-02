65317e4737d31a94a35cfd1c58c29a70ae8d9cc4
nidhijoshi95 <nidhijoshi95@gmail.com>
2022-06-02 08:00:02 +0200

    first_commit

---
+++ model_gacnn.py
@@ -0,0 +1,110 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+class View(nn.Module):
+    """Changes view using a nn.Module."""
+
+    def __init__(self, *shape):
+        super(View, self).__init__()
+        self.shape = shape
+
+    def forward(self, input):
+        # print('Input_view_something: ',input.view(*self.shape))
+        return input.view(*self.shape)
+
+class Net(nn.Module):
+    def __init__(self, num_classes=8):
+              
+        super(Net, self).__init__()
+        
+        self.inputdim = 512
+        self.num_regions = 20       
+        self.base = models.vgg16(pretrained=True).features[:21]    #21 correspond to conv4_2 (9th layer) in vgg16 
+        
+        self.local_attention_block =   nn.ModuleList([  nn.Sequential( 
+                                        nn.MaxPool2d(2),           
+                                        nn.Conv2d(self.inputdim, 128, kernel_size=1),
+                                        nn.ReLU(),
+                                        nn.BatchNorm2d(128),
+                                        View(-1,128*3*3),                                                                                
+                                        nn.Linear(128*3*3,64),
+                                        nn.ReLU(),                                         
+                                        nn.Linear(64,1),                                                                                         
+                                        nn.Sigmoid()) 
+                                       for i in range(self.num_regions)])  
+        
+        self.global_attention_block =    nn.Sequential(nn.MaxPool2d(2),           
+                                        nn.Conv2d(self.inputdim, 128, kernel_size=1),
+                                        nn.ReLU(), 
+                                        nn.BatchNorm2d(128),
+                                        View(-1,128*7*7),                                                                               
+                                        nn.Linear(128*7*7,64),
+                                        nn.ReLU(),                                        
+                                        nn.Linear(64,1),                                                                                         
+                                        nn.Sigmoid() ) 
+                                        
+        self.PG_unit_1 =  nn.ModuleList([ nn.Sequential(          
+                                        nn.Conv2d(512, 512, kernel_size=3, padding = 1),
+                                        nn.ReLU())                  
+                                    for i in range(self.num_regions)])                       
+                                                
+        self.PG_unit_2 =  nn.ModuleList([ nn.Sequential(
+                                        View(-1,512*6*6),          
+                                        nn.Linear(512*6*6, 64), 
+                                        nn.ReLU())                  
+                                    for i in range(self.num_regions)])    
+
+        self.GG_unit_1 =   nn.Sequential(
+                                        nn.MaxPool2d(2),          
+                                        nn.Conv2d(512, 512, kernel_size=3, padding = 1),
+                                        nn.ReLU() )                
+                                                        
+                                                
+        self.GG_unit_2 =  nn.Sequential( 
+                                        View(-1,512*14*14),         
+                                        nn.Linear(512*14*14, 512), 
+                                        nn.ReLU())                  
+                                  
+
+        # self.fc1 = nn.Linear(1792,512)
+        self.fc1 = nn.Linear(512,256)
+        self.fc2 = nn.Linear(256, num_classes)
+                                                                                                
+                                                                                                
+    def pg_unit(self, patch, region_num):        
+        out = self.PG_unit_1[region_num](patch)
+        att_wt = self.local_attention_block[region_num](out)
+        out = self.PG_unit_2[region_num](out)
+        att_out = out * att_wt
+        return att_out 
+
+    def gg_unit(self, patch):
+        temp = self.GG_unit_1(patch)
+        global_wt =  self.global_attention_block(temp)
+        global_features = global_wt * self.GG_unit_2(temp)
+        return global_features          
+               
+    def forward(self, x, landmarks_list): #landmarks list  
+        bs = x.size(0)     
+
+        out = self.base(x)
+
+        landmarks_list[landmarks_list < 3] = 3 
+        landmarks_list[landmarks_list > 20] = 20
+          
+        patches_20 = [[out[i, :, x-3:x+3, y-3:y+3] for x, y in landmarks_list[i]] for i in range(bs)] 
+        patches_20 = torch.stack([torch.stack(patches_20[i], dim=0) for i in range(bs)], dim=0) #bs x 24 x 512 x 6 x 6 
+        local_features = torch.cat([self.pg_unit(patches_20[:, i], i) for i in range(20)], dim=1)
+       
+        global_features = self.gg_unit(out)    
+        # print("Global Features: ",global_features.shape)
+        # print('bs: ',bs)
+        # print('Local Features:',local_features.shape)
+        # input_features = torch.cat((local_features, global_features), dim = 1)
+        input_features = global_features
+        # print('Features: ', input_features.shape)
+        f = self.fc1(input_features)       
+        f = self.fc2(f) 
+        # print(f.shape)
+        return f 
